dfec36fafd7e518a7ce4faa43647458e68c930df
Pavel Kryuchkov <pavkr92@gmail.com>
2023-08-29 10:45:24 +0600

    chore: prepare deploy files

--- src/components/Display/Display.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './styles.module.css';
-
-function Display({ content }) {
-  return <div className={styles.display}>{content}</div>;
-}
-
-export default Display;
--- src/components/Row/Row.jsx
+++
@@ -1,7 +0,0 @@
-import Cell from '../Cell/Cell';
-
-function Row() {
-  return <button>b</button>;
-}
-
-export default Row;
